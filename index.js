69ae64072fcc398c5ec5dbeb796bdd2c0890f4e8
moshiurrahmansourave <moshiurrahmansourave@gmail.com>
2023-11-08 13:08:04 +0600

    set jwt and cors

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(cors({
+  origin:[
+    'http://localhost:5173'
+  ],
+  credentials:true
+}));
 app.use(express.json());
 
 
@@ -33,6 +39,22 @@ async function run() {
 
     const assignmentCollection = client.db('assignmentDB').collection('assignment')
 
+    //auth related
+     app.post('/jwt',async(req,res)=>{
+      const user = req.body;
+      console.log('user for token',user)
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{expiresIn:'2h'})
+
+      res.cookie('token', token,{
+        httpOnly:true,
+        secure:true,
+        sameSite:'none'
+      })
+      .send({success:true})
+     })
+
+
+    //service related
     app.get('/assignment',async (req, res)=>{
         const cursor = assignmentCollection.find();
         const result = await cursor.toArray();
